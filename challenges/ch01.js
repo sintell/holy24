468fe77722ebcb7a10d6a69f0427612c7059035f
Aleksei Anatskii <a.anatskiy@hh.ru>
2024-04-23 13:33:32 +0400

    update challenge and test

--- challenges/ch01.js
+++
@@ -1,9 +0,0 @@
-function myVeryOwnFunction() {
-  const aBcDe_1 = 1;
-  const b = 2;
-  result = aBcDe_1 + b;
-
-  return result + 1;
-}
-
-exports.myVeryOwnFunction = myVeryOwnFunction;
---
+++ test/stack_challenge.test.js
@@ -0,0 +1,38 @@
+const Stack = require("../challenges/stack_challenge");
+const assert = require("assert");
+
+describe("new Stack", () => {
+  it("должен быть пустым, при создании", () => {
+    assert.equal(new Stack().size(), 0);
+  });
+
+  it("должен добавлять элементы по порядку", () => {
+    const myStack = new Stack();
+    const expected = [1, 3, 7];
+    expected.forEach((el) => myStack.add(el));
+
+    assert.equal(myStack.items.length, expected.length);
+    assert.deepStrictEqual(myStack.items, expected);
+  });
+
+  it("должен удалять элементы по порядку", () => {
+    const myStack = new Stack();
+    const expected = [1, 3, 7];
+    expected.forEach((el) => myStack.add(el));
+
+    assert.equal(myStack.remove(), expected.pop());
+    assert.equal(myStack.remove(), expected.pop());
+    assert.equal(myStack.remove(), expected.pop());
+  });
+
+  it("должен возвращать последний элемент при вызове peek()", () => {
+    const myStack = new Stack();
+    const expected = [1, 3, 7];
+    expected.forEach((el) => myStack.add(el));
+
+    assert.equal(myStack.peek(), expected[expected.length - 1]);
+
+    myStack.add(11);
+    assert.equal(myStack.peek(), 11);
+  });
+});
